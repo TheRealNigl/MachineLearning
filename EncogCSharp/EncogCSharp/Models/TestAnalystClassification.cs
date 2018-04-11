16cfac8f1ca79afd5898ff5616120dd88ca3ebb3
Alexander James Nigl <anigl3450@gmail.com>
2018-04-10 19:09:50 -0500

    Adding basic linear regression ML algorithm
    Explored the roots of writing to CSV files

---
+++ EncogCSharp/EncogCSharp/Models/TestAnalystClassification.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using Encog.Util;
+using Encog.Util.CSV;
+using Encog.Util.File;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Encog.App.Analyst
+{
+    public class TestAnalystClassification
+    {
+        public TempDir TEMP_DIR = new TempDir();
+        
+        public void TestClassification()
+        {
+            FileInfo rawFile = TEMP_DIR.CreateFile("simple.csv");
+            FileInfo egaFile = TEMP_DIR.CreateFile("simple.ega");
+            FileInfo outputFile = TEMP_DIR.CreateFile("simple_output.csv");
+
+            FileUtil.CopyResource("Encog.Resources.simple.csv", rawFile);
+            FileUtil.CopyResource("Encog.Resources.simple-c.ega", egaFile);
+
+            EncogAnalyst analyst = new EncogAnalyst();
+            analyst.AddAnalystListener(new ConsoleAnalystListener());
+            analyst.Load(egaFile);
+
+            analyst.ExecuteTask("task-full");
+
+            ReadCSV csv = new ReadCSV(outputFile.ToString(), true, CSVFormat.English);
+            while (csv.Next())
+            {
+                Assert.AreEqual(csv.Get(3), csv.Get(4));
+            }
+
+            Assert.AreEqual(4, analyst.Script.Fields.Length);
+            Assert.AreEqual(3, analyst.Script.Fields[3].ClassMembers.Count);
+
+            csv.Close();
+        }
+    }
+}
---
+++ EncogCSharp/EncogCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EncogCSharp.Models;
+
+namespace EncogCSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------" + "Encog ML Introduction " + "---------------");
+            EncogMLIntroduction encog = new EncogMLIntroduction();
+            encog.Run();
+            Console.WriteLine("----------------------------------------------------------------------\n");
+            Console.WriteLine("Testing The four other function:");
+            TestEncogAnalyst test = new TestEncogAnalyst();
+            test.TestIrisRegressionFF();
+            Console.WriteLine("---------------------------------");
+            Console.ReadLine();
+        }
+    }
+}
